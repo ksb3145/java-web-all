b10a7c79a16935291a8ab0ecdad2e92c708a77a2
saebom <ksb3145@naver.com>
2019-10-16 21:47:21 +0900

    no message

---
+++ java20_servlet_model2/src/member/vo/SessionVO.java
@@ -0,0 +1,26 @@
+package member.vo;
+
+public class SessionVO {
+	private String userId;
+	private String userName;
+	private String userEmail;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+}
